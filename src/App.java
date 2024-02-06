bc908c830f0a1ed53932207016fbc06727bd6e79
al-uminium <aeronafic@gmail.com>
2024-02-06 09:32:41 +0800

    updating

--- src/App.java
+++ src/App.java
@@ -1,8 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         BankAccount newBankAccount = new BankAccount("Alan", (float) 100.0);
+        FixedDepositAccount fa1 = new FixedDepositAccount("Jaret", (float) 150, 4, 8);
         newBankAccount.deposit((float) 100);
         newBankAccount.withdraw((float) 150);
+        System.out.println(newBankAccount.getAccountTransactions());
+        fa1.deposit(0);
+        fa1.withdraw(0);
+        fa1.getBalance();
         newBankAccount.withdraw((float) 150);
     }
 }
